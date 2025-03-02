fa5df2419330c39715d7ab2eab1b216e4f06968f
13Scorpion13 <gdlk74@gmail.com>
2025-03-02 21:46:50 +0300

    feature/task1068

---
+++ src/timus/task1409.java
@@ -0,0 +1,19 @@
+package timus;
+
+import java.util.Scanner;
+
+public class task1409 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int Harry = in.nextInt();
+        int Larry = in.nextInt();
+
+        int all = Harry + Larry - 1;
+        int HarryL = all - Harry;
+        int LarryL = all - Larry;
+
+        System.out.println(HarryL + " " + LarryL);
+    }
+}
