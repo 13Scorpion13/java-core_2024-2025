6a95901f194bd7eaa5fd038c8fd6d63a8622056a
13Scorpion13 <gdlk74@gmail.com>
2025-05-02 15:08:38 +0300

    обновления для лабы 6)

--- src/lr5/Example4.java
+++ src/lr5/Example4.java
@@ -7,5 +7,26 @@ import java.util.stream.Collectors;
 
 public class Example4 {
 
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        Random random = new Random();
 
+        System.out.println("Исходные числа:");
+        for (int i = 0; i < 10; i++) {
+            int num = random.nextInt(100); // чтобы можно было проверить в голове)))
+            numbers.add(num);
+            System.out.print(num + " ");
+        }
+
+        List<Integer> squaredNumbers = squareNumbers(numbers);
+
+        System.out.println("\n\nКвадраты чисел:");
+        squaredNumbers.forEach(n -> System.out.print(n + " "));
+    }
+
+    public static List<Integer> squareNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .map(x -> x * x)
+                .collect(Collectors.toList());
+    }
 }
